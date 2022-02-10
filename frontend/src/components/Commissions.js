ff98b45824b49b1a4d9e83fd0d26d1fcd1d87fcf
Adriana <am20dipi@alum.siena.edu>
2022-02-10 18:16:08 -0500

    Importing images for commissions page; rendering each image; changing the styling

--- frontend/src/components/Commissions.js
+++ frontend/src/components/Commissions.js
@@ -1,7 +1,42 @@
+import Sidenav from './Sidenav.js'
+import ageof from '../images/commissions/ageof.jpeg'
+import bindtruecolors from '../images/commissions/bindtruecolors.png'
+import brokeninrefraction from '../images/commissions/brokeninrefraction.jpg'
+import rulethemall from '../images/commissions/rulethemall.png'
+import lifegoeson from '../images/commissions/lifegoeson.jpg'
+
 function Commissions(){
     return (
-        <div className="container">
-            <h3>commissions</h3>
+        <div className="wrapper" style={{display: 'flex'}}>
+            <section className="left">
+                <Sidenav/>
+            </section>
+            <section className="right">
+            
+                <div className="img-gallery">
+                    <h2 style={{textAlign: 'left'}}>commissions<p className="text-muted" style={{fontSize:'medium', textAlign: 'left' }}></p></h2>
+                    <div style={{cursor: 'pointer'}} id="commission" onClick={() => window.open('https://ageofapocalypseny.bandcamp.com/album/grim-wisdom')}>
+                        <img src={ageof}/>
+                        <p style={{padding: '10px', textAlign: 'center'}} id="painting" className="img__description"> Age of Apocalypse -- Grim Wisdom -- Record Cover --  </p>
+                    </div>
+                    <div style={{cursor: 'pointer'}}  id="commission"onClick={() => window.open('https://pleadyourcaserecords.bandcamp.com/album/pyc008-true-colors')}>
+                        <img src={bindtruecolors}/>
+                        <p style={{padding: '10px', textAlign: 'center'}} id="painting" className="img__description"> Bind -- True colors -- EP cover -- </p>
+                    </div>
+                    <div style={{cursor: 'pointer'}} id="commission" onClick={() => window.open('https://rulethemallhc.bandcamp.com/releases')}>
+                        <img src={rulethemall}/>
+                        <p style={{padding: '10px', textAlign: 'center'}} id="painting" className="img__description"> Rule them all -- blueprint for change -- EP cover -- </p>
+                    </div>
+                    <div style={{cursor: 'pointer'}} id="commission" onClick={() => window.open('https://sanction631.bandcamp.com/album/broken-in-refraction')}>
+                        <img src={brokeninrefraction}/>
+                        <p style={{padding: '10px', textAlign: 'center'}} id="painting" className="img__description">Sanction -- broken in refraction -- Record Cover --  </p>
+                    </div>
+                    <div style={{cursor: 'pointer'}} id="commission" onClick={() => window.open('https://sanction631.bandcamp.com/album/broken-in-refraction')}>
+                        <img src={lifegoeson}/>
+                        <p style={{padding: '10px', textAlign: 'center'}} id="painting" className="img__description">Sanction -- broken in refraction -- Record Cover --  </p>
+                    </div>
+                </div>
+            </section>
         </div>
 
     )
@@ -9,7 +44,8 @@ function Commissions(){
 
 export default Commissions
 
-// Age of Apocalypse 
+// 
 // Bind Life goes on
-// Bind True colors
-// Rule them all blueprint for change
+// 
+// 
+// 
