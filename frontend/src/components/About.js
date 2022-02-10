3f06ea20bcc581685d86b20ad78798e54c0425dd
Adriana <am20dipi@alum.siena.edu>
2022-02-10 14:26:23 -0500

    Alot of carousel work

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,7 +7,7 @@ function About(){
             <p className="text-muted">Modern oil-focused painter based out of New York.</p>
             <br/>
             <img style={{width: '50%'}} src={selfportrait} alt="self portrait"/>
-            <figcaption style={{fontStyle: 'italic'}} className="text-muted">Self portrait. 2020. Oil on canvas board.</figcaption>
+            <figcaption style={{fontStyle: 'italic'}} className="text-muted">Self portrait. Oil on canvas board. 2020.</figcaption>
         </div>
     )
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,39 +2,77 @@ import congruent from '../images/congruent.png'
 import brighterside from '../images/brighterside.png'
 import inbetween from '../images/inbetween.png'
 
+import React, { useState } from 'react'
+
+import Carousel from 'react-bootstrap/carousel'
+
 
 function Home(){
     return (
         <div className="container">
             <h2>DILLON PERINO</h2>
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img class="d-block w-50"  src={congruent} alt="First slide"/>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-50" src={brighterside} alt="Second slide"/>
-                    </div>
-                    <div class="carousel-item">
-                        <img class="d-block w-50" src={inbetween} alt="Third slide"/>
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-                </div>
+            <Carousel className="carousel-fade">
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={congruent}
+                        alt="First slide"
+                        style={{width: '220px'}}
+                    />
+                    <Carousel.Caption style={{color: 'gray'}}>
+                        <h2 style={{fontSize: '30px'}}>Congruent Disillusion</h2>
+                        <p>Mixed media on canvas. 9x12 inches. 2021.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={inbetween}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption style={{color: 'white'}}>
+                        <h2 style={{fontSize: '30px'}}>In between</h2>
+                        <p>Charcoal and acrylic on paper. 18x24 inches. 2018.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={brighterside}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption style={{color: 'white'}}>
+                        <h2 style={{fontSize: '30px'}}>The brighter side of nothing</h2>
+                        <p>Mixed media on canvas. 9x12 inches. 2020.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
 
 export default Home
+
+{/* <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100"  src={congruent} alt="First slide"/>
+                            <div className="carousel-caption d-none d-md-block" style={{color: 'black', fontSize: '40px', fontWeight: 'bold'}}>
+                                <p>Congruent Disillusion</p>
+                                <p>Mixed media on canvas. 9x12 inches. 2021.</p>
+                            </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={brighterside} alt="Second slide"/>
+                        <div className="carousel-caption d-none d-md-block" style={{color: 'black', fontSize: '40px', fontWeight: 'bold'}}>
+                               
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src={inbetween} alt="Third slide"/>
+                        <div className="carousel-caption d-none d-md-block" style={{color: 'black', fontSize: '40px', fontWeight: 'bold'}}>
+                                
+                        </div>
+                    </div>
+                </div> */}
