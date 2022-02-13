307f06827a7b66e49277c6d3edcb1a726ee6536b
Adriana <am20dipi@alum.siena.edu>
2022-02-13 12:17:46 -0500

    Dynamically rendering charcoals and paintings aswell

--- frontend/src/components/Charcoal.js
+++ frontend/src/components/Charcoal.js
@@ -22,9 +22,114 @@ import IMG_9792 from '../images/charcoals/IMG_9792.jpg'
 import IMG_9793 from '../images/charcoals/IMG_9793.jpg'
 import IMG_9794 from '../images/charcoals/IMG_9794.jpg'
 import IMG_9795 from '../images/charcoals/IMG_9795.jpg'
-
 import IMG_9798 from '../images/charcoals/IMG_9798.jpg'
 import IMG_9801 from '../images/charcoals/IMG_9801.jpg'
+
+const charcoals = [
+    {
+        image: standupslow,
+        description: 'stand up slow. Charcoal and acrylic on paper. 11x14 inches. 2019.'
+    },
+    {
+        image: deadeyes,
+        description: 'Untitled. 2018.'
+    },
+    {
+        image: withheld,
+        description: 'Withheld. Pastel and charcoal on paper. 18x24 inches. 2019.'
+    },
+    {
+        image: tonowhere,
+        description: 'To nowhere. Charcoal on paper. 18x24 inches. 2019.'
+    },
+    {
+        image: untilthen,
+        description: 'Until then. Pastel and charcoal on paper. 14x17 inches. 2019.'
+    },
+    {
+        image: celiaathome,
+        description: 'Celia at home. Charcoal and acrylic on paper. 18x24 inches. 2020.'
+    },
+    
+    {
+        image: christalone,
+        description: 'Untitled. 2017.'
+    },
+    {
+        image: disintegratingeden,
+        description: 'Disintegrating eden. 2016.'
+    },
+    {
+        image: emptyroom,
+        description: 'From an empty room I dreamt it was the end. Charcoal and Acrylic on paper. 18x24 inches. 2020.'
+    },
+    {
+        image: IMG_9765,
+        description: 'Charcoal on paper.'
+    },
+    {
+        image: IMG_9768,
+        description: 'Charcoal on paper.'
+    },
+    {
+        image: IMG_9772,
+        description: 'Charcoal on paper.'
+    },
+    {
+        image: IMG_9774,
+        description: 'Charcoal on paper.'
+    },
+    {
+        image: IMG_9781,
+        description: 'Charcoal on paper.'
+    },
+
+    {
+        image: IMG_9783,
+        description: 'Charcoal and pastel on paper.'
+    },
+    {
+        image: IMG_9785,
+        description: 'Charcoal on paper.'
+    },
+    {
+        image: IMG_9788,
+        description: 'Charcoal on paper.'
+    },
+    {
+        image: IMG_9790,
+        description: 'Charcoal on paper.'
+    },
+    {
+        image: IMG_9793,
+        description: 'Public Speaker. Charcoal on paper. 2019.'
+    },
+    {
+        image: IMG_9795,
+        description: 'Charcoal and pastel on paper. 2018.'
+    },
+    {
+        image: IMG_9798,
+        description: 'Charcoal and pastel on paper. 2018.'
+    },
+    {
+        image: IMG_9801,
+        description: 'Charcoal on paper. 2019. '
+    },
+    {
+        image: IMG_9794,
+        description: 'Charcoal on paper. 2019. '
+    },
+    {
+        image: IMG_9792,
+        description: 'Charcoal on paper.'
+    },
+    {
+        image: courage,
+        description: 'Untitled. 2017.'
+    }
+    
+]
 
 
 
@@ -38,106 +143,19 @@ function Charcoal(){
             <section className="right" style={{flex: '90%'}}>
                 <div className="img-gallery">
                     <h1 style={{textAlign: 'left'}}>charcoal<p style={{fontSize:'medium', textAlign: 'left' }} className="text-muted">A collection of charcoal drawings spanning from 2016-present.</p></h1>
-                    <div className="img__wrap">
-                        <img src={standupslow}/>
-                        <p style={{padding: '10px'}} className="img__description">Stand up slow. Charcoal and acrylic on paper. 11x14 inches. 2019.</p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={deadeyes}/>
-                        <p style={{padding: '10px'}} className="img__description">Untitled. 2018.</p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={courage}/>
-                        <p style={{padding: '10px'}} className="img__description">Untitled. 2017.</p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={withheld}/> 
-                        <p style={{padding: '10px'}} className="img__description">Withheld. Pastel and charcoal on paper. 18x24 inches. 2019.</p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={tonowhere}/>
-                        <p style={{padding: '10px'}} className="img__description">To nowhere. Charcoal on paper. 18x24 inches. 2019.</p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={untilthen}/>
-                        <p style={{padding: '10px'}} className="img__description">Until then. Pastel and charcoal on paper. 14x17 inches. 2019.</p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={celiaathome}/>
-                        <p style={{padding: '10px'}} className="img__description">Celia at home. Charcoal and acrylic on paper. 18x24 inches. 2020.</p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={christalone}/>
-                        <p style={{padding: '10px'}} className="img__description">Untitled. 2017.</p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={disintegratingeden}/>
-                        <p style={{padding: '10px'}} className="img__description">Disintegrating eden. 2016. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={emptyroom}/>
-                        <p style={{padding: '10px'}} className="img__description">From an empty room I dreamt it was the end. Charcoal and Acrylic on paper. 18x24 inches. 2020.</p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9765}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9768}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9772}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9774}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9781}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9783}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal and pastel on paper. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9785}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9788}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9790}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9793}/>
-                        <p style={{padding: '10px'}} className="img__description">Public Speaker. Charcoal on paper. 2019. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9795}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal and pastel on paper. 2018. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9798}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal and pastel on paper. 2018. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9801}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. 2019. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9794}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. 2019. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={IMG_9792}/>
-                        <p style={{padding: '10px'}} className="img__description">Charcoal on paper. </p>
-                    </div>
+                    {charcoals.map((charcoal, index) => {
+                        return (
+
+                            <div className="img__wrap" key={index}>
+                                <img src={charcoal.image}/>
+                                <p style={{padding: '10px'}} className="img__description">{charcoal.description} </p>
+                            </div>
+                        )
+                    })}
+        
+               
+
+            
                 </div>
             </section>
 
--- frontend/src/components/Paintings.js
+++ frontend/src/components/Paintings.js
@@ -3,6 +3,23 @@ import brighterside from '../images/brighterside.png'
 import congruent from '../images/congruent.png'
 import selfportrait from '../images/selfportrait.png'
 
+const paintings = [
+    {
+        image: congruent,
+        description: 'Congruent. Mixed media on canvas. 9x12 inches. 2021.',
+        
+    },
+    {
+        image: brighterside,
+        description: 'The brighter side of nothing. Mixed media on canvas. 9x12 inches. 2020.',
+    },
+    {
+        image: selfportrait,
+        description: 'Self portrait. Oil on canvas board. 2020.',
+        
+    }
+]
+
 function Paintings(){
     return(
         <div className="wrapper" style={{display: 'flex'}}>
@@ -13,18 +30,15 @@ function Paintings(){
             
                 <div className="img-gallery">
                     <h1 style={{textAlign: 'left'}}>oil<p className="text-muted" style={{fontSize:'medium', textAlign: 'left' }}>A deeper dive into my favorite medium.</p></h1>
-                    <div className="img__wrap">
-                        <img src={congruent}/>
-                        <p style={{padding: '10px', textAlign: 'center'}} id="painting" className="img__description">Congruent. Mixed media on canvas. 9x12 inches. 2021.  </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={brighterside}/>
-                        <p style={{padding: '10px', textAlign: 'center'}}  id="painting" className="img__description">The brighter side of nothing. Mixed media on canvas. 9x12 inches. 2020. </p>
-                    </div>
-                    <div className="img__wrap">
-                        <img src={selfportrait}/>
-                        <p style={{padding: '10px', textAlign: 'center'}} id="painting"  className="img__description">Self portrait. Oil on canvas board. 2020. </p>
-                    </div>
+                    {paintings.map((painting, index) => {
+                        return (
+                            <div className="img__wrap">
+                                <img src={painting.image}/>
+                                <p style={{padding: '10px', textAlign: 'center'}} id="painting" className="img__description"> {painting.description} </p>
+                            </div>
+                        )
+                    })}
+                   
                 </div>
             </section>
         </div>
